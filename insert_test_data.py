eeb7bd35fc3c94c6ca35ebaf2c9130db30b74575
Nikita Andreev <nullius.res@gmail.com>
2022-05-10 16:44:26 +0300

    add test data

---
+++ insert_test_data.py
@@ -0,0 +1,62 @@
+from data_layer import new_user
+
+data = [
+  {
+    'uid': 0,
+    'name': 'Гомер',
+    'surname': 'Симпсон',
+    'position': 'Оператор',
+    'phone': '111-111-1',
+    'username': 'OpenBuddyBot',
+    'active': 1,
+  },
+  {
+    'uid': 1,
+    'name': 'мистер',
+    'surname': 'Бернс',
+    'position': 'Босс',
+    'phone': '111-111-2',
+    'username': 'OpenBuddyBot',
+    'active': 1,
+  },
+  {
+    'uid': 2,
+    'name': 'Сеймур',
+    'surname': 'Скиннер',
+    'position': 'Учитель',
+    'phone': '111-111-3',
+    'username': 'OpenBuddyBot',
+    'active': 1,
+  },
+  {
+    'uid': 3,
+    'name': 'Барт',
+    'surname': 'Симпсон',
+    'position': 'Ученик',
+    'phone': '111-111-4',
+    'username': 'OpenBuddyBot',
+    'active': 1,
+  },
+  {
+    'uid': 4,
+    'name': 'Барни',
+    'surname': 'Гамбл',
+    'position': 'Собутыльник',
+    'phone': '111-111-5',
+    'username': 'OpenBuddyBot',
+    'active': 1,
+  },
+  {
+    'uid': 5,
+    'name': 'Мо',
+    'surname': 'Сизлак',
+    'position': 'Бармен',
+    'phone': '111-111-6',
+    'username': 'OpenBuddyBot',
+    'active': 1,
+  },
+]
+
+if __name__ == '__main__':
+  for user in data:
+    new_user(user)
