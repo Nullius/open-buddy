26236b5ca1ac4527bd6d7e329e07ceae8d156b04
Nikita Andreev <nullius.res@gmail.com>
2022-05-10 08:21:33 +0300

    buddy fix

--- __init__.py
+++ __init__.py
@@ -3,9 +3,10 @@ import sys
 import logging
 
 from aiogram import Bot, Dispatcher, executor, types
-from dotenv import load_dotenv
+from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters.state import State, StatesGroup
 
-import new_buddy
+from dotenv import load_dotenv
 
 load_dotenv()
 bot_token = os.getenv("BUDDY_TOKEN")
@@ -27,6 +28,50 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(lambda message: message.text == "Test")
 async def test_handler(message: types.Message):
     await message.reply("Pushed")
+
+class Buddy(StatesGroup):
+  name = State()
+  position = State()
+  phone = State()
+
+@dp.message_handler(commands='new_buddy')
+async def name_start(message: types.Message):
+  await Buddy.name.set()
+  await message.answer("Ваше имя и фамилия:")
+
+@dp.message_handler(state='*', commands='cancel')
+async def cancel_handler(message: types.Message, state: FSMContext):
+  current_state = await state.get_state()
+  if current_state is None:
+    return
+  
+  await state.finish()
+  await message.reply('Отменено', reply_markup=types.ReplyKeyboardRemove())
+
+@dp.message_handler(state=Buddy.name)
+async def process_name(message: types.Message, state: FSMContext):
+  async with state.proxy() as data:
+    data['name'] = message.text
+
+  await Buddy.next()
+  await message.reply("Ваша должность:")
+
+@dp.message_handler(state=Buddy.position)
+async def process_position(message: types.Message, state=FSMContext):
+  await Buddy.next()
+  await state.update_data(position=message.text)
+  await message.reply("Ваш телефон:")
+
+@dp.message_handler(state=Buddy.phone)
+async def process_phone(message: types.Message, state=FSMContext):
+  async with state.proxy() as data:
+    data['phone'] = message.text
+    data['uid'] = message.chat.id
+    await bot.send_message(
+      message.chat.id,
+      'Finished!'
+    )
+    await state.finish()
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
