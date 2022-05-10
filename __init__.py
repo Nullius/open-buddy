44d7a86a64c7e3d0170390a6ca76daedf4b45c68
Nikita Andreev <nullius.res@gmail.com>
2022-05-10 07:07:37 +0300

    dotenv token

--- __init__.py
+++ __init__.py
@@ -1,7 +1,19 @@
+import os
+import sys
 import logging
-from aiogram import Bot, Dispatcher, executor, types
 
-bot = Bot(token="")
+from aiogram import Bot, Dispatcher, executor, types
+from dotenv import load_dotenv
+
+load_dotenv()
+bot_token = os.getenv("BUDDY_TOKEN")
+if not bot_token:
+    sys.exit("Error: no token")
+
+
+bot = Bot(token=bot_token)
+dp = Dispatcher(bot)
+logging.basicConfig(level=logging.INFO)
 
 @dp.message_handler(commands="start")
 async def cmd_start(message: types.Message):
