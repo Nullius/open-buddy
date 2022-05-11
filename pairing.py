2fb89a4a062b18e3b8512f2e172bc6b9c9faa9bf
Nikita Andreev <nullius.res@gmail.com>
2022-05-11 06:59:35 +0300

    pairing prototype

--- pairing.py
+++ pairing.py
@@ -3,4 +3,17 @@ import random
 from data_layer import get_users
 
 if __name__ == '__main__':
-  print(get_users())
+  users = get_users()
+  print(users)
+  uids = [user[0] for user in users]
+
+  if (len(uids) % 2 == 1):
+    lone = uids.pop()
+    # TODO send message
+
+  while len(uids) > 0:
+    print(uids)
+    to_pair = uids.pop(0)
+    buddy = random.choice(uids)
+    uids.remove(buddy)
+    print(to_pair, buddy)
