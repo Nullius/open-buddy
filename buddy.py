94753740e8729ba7520a7c6022e42194ef7bde5f
Nikita Andreev <nullius.res@gmail.com>
2022-05-24 09:14:25 +0300

    fix cancel

--- buddy.py
+++ buddy.py
@@ -136,7 +136,7 @@ async def process_phone(message: types.Message, state=FSMContext):
     await bot.send_message(
       message.chat.id,
       replies.finish,
-      reply_markup=types.ReplyKeyboardRemove
+      reply_markup=types.ReplyKeyboardRemove()
     )
     await state.finish()
 
